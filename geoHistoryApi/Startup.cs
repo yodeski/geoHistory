a428027efff9846c4198634a184145a0dee22168
Yodeski Rodriguez <yodeski_rodriguez@partech.com>
2017-08-03 09:16:46 -0400

    '2'

--- geoHistoryApi/Startup.cs
+++ geoHistoryApi/Startup.cs
@@ -61,7 +61,7 @@ namespace geoHistoryApi
             app.UseStaticFiles(new StaticFileOptions()
             {
                 FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Public")),
-                RequestPath = new PathString("/public")
+                RequestPath = new PathString("/Public")
             });
 
             app.UseMvc(routes =>
